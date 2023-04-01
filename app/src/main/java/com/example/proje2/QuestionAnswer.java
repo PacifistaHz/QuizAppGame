5b578ab650f0a83af08b9cd4a265dfc7068c77c4
PC <hzgratse@gmail.com>
2023-04-01 10:26:25 +0300

    ikinci gönderim

---
+++ app/src/main/java/com/example/proje2/QuestionAnswer.java
@@ -0,0 +1,44 @@
+package com.example.proje2;
+
+public class QuestionAnswer {
+
+    public static String question[] ={
+            "Which actress has won the most Oscars?",
+            "Which state is the biggest in the US?",
+            "Name the Largest Dam of the World?",
+            "What is the name of our galaxy?",
+            "Which planet in the Solar System is the smallest?",
+            "Which month has 28 days?",
+            "Which country's flag created first?",
+            "What was the first woman name?",
+            "What is the name of the first teacher?",
+            "Which country has the longest winters?"
+    };
+
+    public static String choices[][] = {
+            {"Katharine Hepburn","Olivia Colman","Nicole Kidman","Andra Day"},
+            {"Texas","Arizona","California","Alaska"},
+            {"Ataturk","Three Gorges","Tarbela","Oahe"},
+            {"Andromeda ","The Milky Way","Virgo A","Cygnus A"},
+            {"Pluto","Earth","Mars","Mercury"},
+            {"February","November","June","All"},
+            {"USA","Italy","Denmark","Russia"},
+            {"Eva","Lily","Emma","Emily"},
+            {"Paulino Herbel","Georgina Stephano","Eva Rodruguez","Savitribai Phule"},
+            {"Canada","Finland","Norway","Russia"}
+
+    };
+
+    public static String correctAnswers[] = {
+            "Katharine Hepburn",
+            "Alaska",
+            "Tarbela",
+            "The Milky Way",
+            "Mercury",
+            "All",
+            "Denmark",
+            "Eva",
+            "Savitribai Phule",
+            "Finland"
+    };
+}
